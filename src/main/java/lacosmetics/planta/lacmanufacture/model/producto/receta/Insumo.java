5025a9b6460df5fbc8754b774ed727b4f9a985bc
RaulEst50 <raulest50@gmail.com>
2025-08-21 15:15:32 -0500

    Ignore unknown JSON fields in Insumo

--- src/main/java/lacosmetics/planta/lacmanufacture/model/producto/receta/Insumo.java
+++ src/main/java/lacosmetics/planta/lacmanufacture/model/producto/receta/Insumo.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name="insumos")
@@ -13,6 +14,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Insumo {
 
     @Id
