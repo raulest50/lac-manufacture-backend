4a5e08031ee7b293530f7f0aa52bc76693c08236
esteban_laptop_exotic <raulest50@gmail.com>
2025-12-09 10:16:56 -0500

    agregue un modelo para guardar snapshotsde los insumos cuando estos son actualizados

--- src/main/java/lacosmetics/planta/lacmanufacture/model/producto/manufacturing/snapshots/ManufacturingVersion.java
+++ src/main/java/lacosmetics/planta/lacmanufacture/model/producto/manufacturing/snapshots/ManufacturingVersion.java
@@ -1,7 +1,43 @@
 package lacosmetics.planta.lacmanufacture.model.producto.manufacturing.snapshots;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lacosmetics.planta.lacmanufacture.model.producto.Producto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class ManufacturingVersion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //terminado o semiterminado
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "producto_id")
+    @JsonIgnore
+    private Producto producto;
+
+    private int versionNumber;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String insumosJson;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String procesoProduccionJson;
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    private String casePackJson;
 
 }
